b41aa50493dbd79e3dd0046ecbbc9ae46701ab90
Divyaa01 <divya73908@gmail.com>
2024-09-16 15:11:07 +0530

    adding server setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from './database/db.js';
+
+
+dotenv.config();
+
+
+
+
+
+
+
+
+const app = express()
+
+const port =process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send("server is running");
+});
+
+app.listen(port , () =>{
+    console.log(`your server is running on http://localhost:${port}`);
+    connectDb();
+});
